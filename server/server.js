54737239b1869358223a4b452feb0925de9e90a2
Thieu Dinh Nam <thieudinhnam@marijus-MacBook-Air.local>
2023-04-08 18:47:06 +0700

    configuring server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import chats from './data/data.js';
+import connectDb from './config/db.js';
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Successfully api');
+connectDb();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200,
+};
+
+app.get('/api/chats', cors(corsOptions), (req, res) => {
+    res.status(200).json(chats);
 });
 
-app.listen(8080, () => {
-    console.log('App is running in port: 8080');
+app.listen(PORT, () => {
+    console.log(`App is running in port: ${PORT}`);
 });
