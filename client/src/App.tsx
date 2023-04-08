83171069d34d7322a152bb11a4a4f073ca6e44a7
Thieu Dinh Nam <thieudinhnam@marijus-MacBook-Air.local>
2023-04-08 18:58:19 +0700

    add library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
-    return <div className='App'>App</div>;
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import ChatPage from './pages/ChatPage';
+import LoginPage from './components/Login';
+import RegisterPage from './components/Register';
+
+const router = createBrowserRouter([
+    {
+        path: '/login',
+        element: <LoginPage />,
+    },
+    {
+        path: '/register',
+        element: <RegisterPage />,
+    },
+    {
+        path: '/chats',
+        element: <ChatPage />,
+    },
+]);
+
+const App: React.FC = () => {
+    return (
+        <div className='App'>
+            <RouterProvider router={router} />
+        </div>
+    );
+};
 
 export default App;
