916e612318d320b9d824e9ea1041249c024dba72
Thieu Dinh Nam <thieudinhnam@marijus-MacBook-Air.local>
2023-04-09 18:21:20 +0700

    design schema

---
+++ server/models/chatModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const chatSchema = new mongoose.Schema(
+    {
+        chatName: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        isGroupChat: {
+            type: Boolean,
+            default: false,
+        },
+        users: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        lastestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message',
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+export default Chat;
